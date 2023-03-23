ccc45a4e582ef55e3ad3c7105f10021c3ad28e94
sangldq0321 <sangldq0321@gmail.com>
2023-03-23 13:31:41 +0700

    Fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -142,65 +142,61 @@
     <header>
         <nav class="navbar navbar-expand-lg shadow bg-gradient">
             <div class="container-fluid">
-                <a class="navbar-brand me-0 me-lg-3 fw-bold" href="/"><span
-                        class="d-flex align-items-center"><img class="me-2" src="/assets/img/logo.svg"
-                            width="40px" height="auto">{!! __('Tai Loc Hostel') !!}</span></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                <a class="navbar-brand me-0 me-lg-3 fw-bold" href="/"><span class="d-flex align-items-center"><img
+                            class="me-2" src="/assets/img/logo.svg" width="40px" height="auto">{!! __('Tai Loc Hostel')
+                        !!}</span></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarToggler">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="/"><i
-                                    class="fa-solid fa-house me-2"></i>{!! __('Home') !!}</a>
+                            <a class="nav-link" href="/"><i class="fa-solid fa-house me-2"></i>{!! __('Home') !!}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/rooms/all"><i
-                                    class="fa-solid fa-door-open me-2"></i>{!! __('Rooms') !!}</a>
+                            <a class="nav-link" href="/rooms/all"><i class="fa-solid fa-door-open me-2"></i>{!!
+                                __('Rooms') !!}</a>
                         </li>
                         @auth
-                            <li class="nav-item">
-                                <a class="nav-link" href="/admin"><i
-                                        class="fa-solid fa-chart-line me-2"></i>Dashboard</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/admin/manage/rooms"><i
-                                        class="fa-solid fa-list-check me-2"></i>Manage rooms</a>
-                            </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin"><i class="fa-solid fa-chart-line me-2"></i>Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/admin/manage/rooms"><i
+                                    class="fa-solid fa-list-check me-2"></i>Manage rooms</a>
+                        </li>
                         @endauth
                     </ul>
                     @guest
-                        <form class="d-none d-lg-flex rounded-2 w-50" role="search" action="/search">
-                            <div class="input-group">
-                                <input class="form-control" type="search" placeholder="{!! __('Search') !!}"
-                                    aria-label="{!! __('Search') !!}" name="keyword">
-                                <button class="btn btn-success" type="submit"><i
-                                        class="fa-solid fa-magnifying-glass"></i></button>
-                            </div>
-                        </form>
+                    <form class="d-none d-lg-flex rounded-2 w-50" role="search" action="/search">
+                        <div class="input-group">
+                            <input class="form-control" type="search" placeholder="{!! __('Search') !!}"
+                                aria-label="{!! __('Search') !!}" name="keyword">
+                            <button class="btn btn-success" type="submit"><i
+                                    class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
+                    </form>
                     @endguest
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown me-0 me-lg-2">
                             <a class="nav-link dropdown-toggle" href="javascript:void(0)" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
                                 @if (app()->getLocale() == 'vi')
-                                    <img src="/assets/img/vn.svg" style="width: auto;height:1.25rem;">
+                                <img src="/assets/img/vn.svg" style="width: auto;height:1.25rem;">
                                 @else
-                                    <img src="/assets/img/us.svg" style="width: auto;height:1.25rem;">
+                                <img src="/assets/img/us.svg" style="width: auto;height:1.25rem;">
                                 @endif
                             </a>
                             <ul class="dropdown-menu dropdown-menu-lg-end">
                                 <li><a class="dropdown-item" href="/lang/vi">
                                         @if (app()->getLocale() == 'vi')
-                                            <span class="fw-bold d-flex align-items-center"><img class="me-2"
-                                                    src="/assets/img/vn.svg" style="width: auto;height:1.25rem;">VN<i
-                                                    class="fa-solid fa-check ms-2 text-success"></i></span>
+                                        <span class="fw-bold d-flex align-items-center"><img class="me-2"
+                                                src="/assets/img/vn.svg" style="width: auto;height:1.25rem;">VN<i
+                                                class="fa-solid fa-check ms-2 text-success"></i></span>
                                         @else
-                                            <span class="d-flex align-items-center"><img class="me-2"
-                                                    src="/assets/img/vn.svg"
-                                                    style="width: auto;height:1.25rem;">VN</span>
+                                        <span class="d-flex align-items-center"><img class="me-2"
+                                                src="/assets/img/vn.svg" style="width: auto;height:1.25rem;">VN</span>
                                         @endif
                                     </a></li>
                                 <li>
@@ -208,43 +204,41 @@
                                 </li>
                                 <li><a class="dropdown-item" href="/lang/en">
                                         @if (app()->getLocale() == 'en')
-                                            <span class="fw-bold d-flex align-items-center"><img class="me-2"
-                                                    src="/assets/img/us.svg" style="width: auto;height:1.25rem;">EN<i
-                                                    class="fa-solid fa-check ms-2 text-success"></i></span>
+                                        <span class="fw-bold d-flex align-items-center"><img class="me-2"
+                                                src="/assets/img/us.svg" style="width: auto;height:1.25rem;">EN<i
+                                                class="fa-solid fa-check ms-2 text-success"></i></span>
                                         @else
-                                            <span class="d-flex align-items-center"><img class="me-2"
-                                                    src="/assets/img/us.svg"
-                                                    style="width: auto;height:1.25rem;">EN</span>
+                                        <span class="d-flex align-items-center"><img class="me-2"
+                                                src="/assets/img/us.svg" style="width: auto;height:1.25rem;">EN</span>
                                         @endif
                                     </a></li>
                             </ul>
                         </li>
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="/login"><i
-                                        class="fa-solid fa-right-to-bracket me-2"></i>{!! __('Log in') !!}</a>
-                            </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/login"><i class="fa-solid fa-right-to-bracket me-2"></i>{!!
+                                __('Log in') !!}</a>
+                        </li>
                         @endguest
                         @auth
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="javascript:void(0)" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false"><span class="me-2"><img
-                                            src="/assets/img/img.svg"
-                                            data-src="/assets/img/avatar/{{ Auth::user()->avatar }}" class="me-2 lazy"
-                                            width="32" height="auto"
-                                            alt="Avatar">{{ Auth::user()->fullname }}</span></a>
-                                <ul class="dropdown-menu dropdown-menu-lg-end">
-                                    <li><a class="dropdown-item" href="/user/view/{{ Auth::user()->id }}"><i
-                                                class="fa-solid fa-user me-2"></i>{!! __('View profile') !!}</a>
-                                    </li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="/logout"><i
-                                                class="fa-solid fa-right-from-bracket me-2"></i>{!! __('Log out') !!}</a>
-                                    </li>
-                                </ul>
-                            </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="javascript:void(0)" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false"><span class="me-2"><img
+                                        src="/assets/img/img.svg"
+                                        data-src="/assets/img/avatar/{{ Auth::user()->avatar }}" class="me-2 lazy"
+                                        width="32" height="auto" alt="Avatar">{{ Auth::user()->fullname }}</span></a>
+                            <ul class="dropdown-menu dropdown-menu-lg-end">
+                                <li><a class="dropdown-item" href="/user/view/{{ Auth::user()->id }}"><i
+                                            class="fa-solid fa-user me-2"></i>{!! __('View profile') !!}</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="/logout"><i
+                                            class="fa-solid fa-right-from-bracket me-2"></i>{!! __('Log out') !!}</a>
+                                </li>
+                            </ul>
+                        </li>
                         @endauth
                     </ul>
                 </div>
@@ -254,28 +248,30 @@
     <main class="flex-shrink-0">
         <div class="container mt-4 mb-5">
             @guest
-                <form class="d-flex d-lg-none mb-3" role="search" action="/search">
-                    <div class="input-group border border-dark rounded-2">
-                        <input class="form-control" type="search" placeholder="{!! __('Search') !!}"
-                            aria-label="{!! __('Search') !!}" name="keyword">
-                        <button class="btn btn-success" type="submit"><i
-                                class="fa-solid fa-magnifying-glass"></i></button>
-                    </div>
-                </form>
+            <form class="d-flex d-lg-none mb-3" role="search" action="/search">
+                <div class="input-group border border-dark rounded-2">
+                    <input class="form-control" type="search" placeholder="{!! __('Search') !!}"
+                        aria-label="{!! __('Search') !!}" name="keyword">
+                    <button class="btn btn-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </form>
             @endguest
             <div class="d-flex justify-content-center">
                 <div class="alert alert-danger">
-                    <div><i class="fa-solid fa-triangle-exclamation me-2"></i>{!! __('Translation for <b>VN</b> is not complete') !!} !</div>
+                    <div><i class="fa-solid fa-triangle-exclamation me-2"></i>{!! __('Translation for <b>VN</b> is not
+                        complete') !!} !</div>
                 </div>
             </div>
             <div class="dropup">
                 <button class="animated-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i
                         class="fa-solid fa-comment"></i></button>
                 <ul class="dropdown-menu dropdown-menu-lg-end" id="floatingBtnShow">
-                    <li><a href="https://www.messenger.com/t/100049253115031" target="_blank" class="dropdown-item p-2 d-flex align-items-center"><img class="me-2" src="/assets/img/messenger.svg" width="56px"
-                                height="auto">Messenger</a></li>
-                    <li><a href="https://zalo.me/0886623971" target="_blank" class="dropdown-item p-2 d-flex align-items-center"><img class="me-2" src="/assets/img/zalo.svg" width="56px"
-                                height="auto">Zalo</a></li>
+                    <li><a href="https://www.messenger.com/t/100049253115031" target="_blank"
+                            class="dropdown-item p-2 d-flex align-items-center"><img class="me-2"
+                                src="/assets/img/messenger.svg" width="56px" height="auto">Messenger</a></li>
+                    <li><a href="https://zalo.me/0886623971" target="_blank"
+                            class="dropdown-item p-2 d-flex align-items-center"><img class="me-2"
+                                src="/assets/img/zalo.svg" width="56px" height="auto">Zalo</a></li>
                 </ul>
             </div>
             @yield('content')
@@ -298,35 +294,36 @@
     <footer class="mt-auto py-4 text-light bg-gradient">
         <div class="container mb-3">
             @guest
-                <div class="row row-cols-1 row-cols-lg-3 g-3">
+            <div class="row row-cols-1 row-cols-lg-3 g-3">
                 @endguest
                 @auth
-                    <div class="row row-cols-1 row-cols-lg-4 g-3">
+                <div class="row row-cols-1 row-cols-lg-4 g-3">
                     @endauth
                     <div class="col text-center text-lg-start">
                         <img class="mb-2" src="/assets/img/logo.svg" width="40px" height="auto">
                         <h5 class="fw-bold mb-2" style="color:rgba(248, 177, 21, 1);">{!! __('Tai Loc Hostel') !!}</h5>
-                        <p class="mb-0"><b>{!! __('Tai Loc Hostel') !!}</b> {!! __('is a hostel for <b>students</b> with <b>good price</b>') !!}.</p>
+                        <p class="mb-0"><b>{!! __('Tai Loc Hostel') !!}</b> {!! __('is a hostel for <b>students</b> with
+                            <b>good price</b>') !!}.</p>
                     </div>
                     <div class="col text-center text-lg-start">
                         <h5 class="fw-bold" style="color:rgba(248, 177, 21, 1);">{!! __('Rooms') !!}:</h5>
                         @guest
-                            <div class="mb-2"><a href="/rooms/all">{!! __('All rooms') !!}</a></div>
-                            <div class="mb-2"><a href="/rooms/available">{!! __('Available rooms') !!}</a></div>
-                            <div><a href="/rooms/rented">{!! __('Rented rooms') !!}</a></div>
+                        <div class="mb-2"><a href="/rooms/all">{!! __('All rooms') !!}</a></div>
+                        <div class="mb-2"><a href="/rooms/available">{!! __('Available rooms') !!}</a></div>
+                        <div><a href="/rooms/rented">{!! __('Rented rooms') !!}</a></div>
                         @endguest
                         @auth
-                            <div class="mb-2"><a href="/admin/manage/rooms">Manage rooms</a></div>
-                            <div><a href="/rooms/modify/add">Add rooms</a></div>
+                        <div class="mb-2"><a href="/admin/manage/rooms">Manage rooms</a></div>
+                        <div><a href="/rooms/modify/add">Add rooms</a></div>
                         @endauth
                     </div>
                     @auth
-                        <div class="col text-center text-lg-start">
-                            <h5 class="fw-bold" style="color:rgba(248, 177, 21, 1);">{!! __('Account') !!}:</h5>
-                            <div class="mb-2"><a href="/user/view/{{ Auth::user()->id }}">{!! __('View profile') !!}</a>
-                            </div>
-                            <div><a href="/logout">{!! __('Log out') !!}</a></div>
+                    <div class="col text-center text-lg-start">
+                        <h5 class="fw-bold" style="color:rgba(248, 177, 21, 1);">{!! __('Account') !!}:</h5>
+                        <div class="mb-2"><a href="/user/view/{{ Auth::user()->id }}">{!! __('View profile') !!}</a>
                         </div>
+                        <div><a href="/logout">{!! __('Log out') !!}</a></div>
+                    </div>
                     @endauth
                     <div class="col text-center text-lg-start">
                         <h5 class="fw-bold" style="color:rgba(248, 177, 21, 1);">{!! __('Maps') !!}:</h5>
@@ -355,7 +352,7 @@
     <script>
         document.addEventListener("DOMContentLoaded", yall);
     </script>
-    <script script src="/assets/js/main.js"></script>
+    <script src="/assets/js/main.js"></script>
     <script src="/assets/js/datatables.min.js"></script>
     <script src="/assets/js/pdfmake.min.js"></script>
     <script src="/assets/js/vfs_fonts.js"></script>
